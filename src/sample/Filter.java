88c01d4c9bdd853bd4cd03f244d3bd7f08f0b16c
Diego Tabares <dtabares@gmail.com>
2019-04-12 14:55:54 -0300

    prueba de filtro de la media

--- src/sample/Filter.java
+++ src/sample/Filter.java
@@ -4,41 +4,12 @@ import java.awt.image.BufferedImage;
 public class Filter {
 
     public BufferedImage applyMeanFilter(BufferedImage bimg, int maskSize){
-        int rgb,r,g,b;
-        BufferedImage result = bimg;
         Mask mask = new Mask(maskSize);
         mask.setMeanMask();
-        System.out.println("Size: " + bimg.getWidth() + " x " + bimg.getHeight());
-        double value = mask.getMatrix()[mask.getCenterX()][mask.getCenterY()];
-        for (int i = 0; i < bimg.getWidth() - maskSize; i++){
-            for (int j = 0; j < bimg.getHeight() - maskSize; j++){
-                    rgb = bimg.getRGB(mask.getCenterX(), mask.getCenterY());
-                    r = ColorUtilities.getRed(rgb);
-                    g = ColorUtilities.getGreen(rgb);
-                    b = ColorUtilities.getBlue(rgb);
-                    System.out.print("Position: " + i + "," + j);
-                    System.out.print(" Changing R: " + r + " B: " + b + " G: " + g);
-                    r = (int) (r + (r * value));
-                    g = (int) (g + (g * value));
-                    b = (int) (b + (b * value));
-                    //Algunos valores se van de rango, pongo esta validacion para truncarlos, no se si esta bien
-                    if(r>255){
-                        r = 255;
-                    }
-                    if(g>255){
-                        g = 255;
-                    }
-                    if(b>255){
-                        b = 255;
-                    }
-                    System.out.println(" To R: " + r + " B: " + b + " G: " + g);
-                    result.setRGB(i,j, ColorUtilities.createRGB(r,g,b));
-                    mask.moveY();
-            }
-            mask.restetY();
-            mask.moveX();
-        }
+        BufferedImage result = applyConvolution(bimg,mask);
+
         return result;
+
     }
 
     public BufferedImage applyMedianFilter(BufferedImage bimg, int maskSize){
@@ -58,6 +29,49 @@ public class Filter {
 
     public  BufferedImage enhance(BufferedImage bimg){
         BufferedImage result = new BufferedImage(bimg.getWidth(),bimg.getHeight(),bimg.getType());
+        return result;
+    }
+
+    private BufferedImage applyConvolution(BufferedImage bimg, Mask mask){
+        int rgb, red, green, blue;
+        BufferedImage result = bimg;
+        int widthLimit = bimg.getWidth() - mask.getSize();
+        int heightLimit = bimg.getHeight() - mask.getSize();
+        Image temp = new Image(bimg);
+        int[][] redChannel = temp.getRedDataMatrixChannel();
+        int[][] greenChannel = temp.getGreenDataMatrixChannel();
+        int[][] blueChannel = temp.getBlueDataMatrixChannel();
+        mask.setMeanMask();
+        red = 0;
+        green = 0;
+        blue = 0;
+
+        for (int i = 0; i <= widthLimit; i++) {
+            for (int j = 0; j <= heightLimit; j++) {
+                for (int k = 0; k < mask.getSize(); k++) {
+                    for (int l = 0; l < mask.getSize(); l++) {
+                        rgb = bimg.getRGB(i+k,j+l);
+                        red += (ColorUtilities.getRed(rgb) * mask.getValue(k,l));
+                        green += (ColorUtilities.getGreen(rgb) * mask.getValue(k,l));
+                        blue += (ColorUtilities.getBlue(rgb) * mask.getValue(k,l));
+                    }
+                }
+                redChannel[i + mask.getCenter()][j + mask.getCenter()] = red;
+                greenChannel[i + mask.getCenter()][j + mask.getCenter()] = green;
+                blueChannel[i + mask.getCenter()][j + mask.getCenter()] = blue;
+                red = 0;
+                green = 0;
+                blue = 0;
+            }
+        }
+
+        // Relleno la imagen a devolver
+        for (int i = 0; i < result.getWidth(); i++) {
+            for (int j = 0; j < result.getHeight(); j++) {
+                result.setRGB(i,j,ColorUtilities.createRGB(redChannel[i][j], greenChannel[i][j], blueChannel[i][j]));
+            }
+        }
+
         return result;
     }
 

1a9a9ef64795c45b851cfb4b63bfc3207602887f
Diego Tabares <dtabares@gmail.com>
2019-05-29 21:02:55 -0300

    Hago privados los attrs y les agrego getters y constructor

--- src/sample/Pixel.java
+++ src/sample/Pixel.java
@@ -2,9 +2,27 @@ package sample;
 
 public class Pixel {
 
-    public int x;
-    public int y;
-    public int value;
+    private int x;
+    private int y;
+    private int value;
+
+    public Pixel(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getValue() {
+        return value;
+    }
 
     public boolean equals(Pixel p){
         return (p.x == x && p.y == y && p.value == value);
